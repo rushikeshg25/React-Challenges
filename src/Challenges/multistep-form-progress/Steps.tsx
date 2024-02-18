818d30d5874e5d8b83c2b044a8cb0869c8738fbe
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-02-18 21:41:45 +0530

    Added Steps

--- src/Challenges/multistep-form-progress/Steps.tsx
+++ src/Challenges/multistep-form-progress/Steps.tsx
@@ -6,13 +6,15 @@ type StepT = {
 const Steps = (props: { steps: StepT[] }) => {
   console.log(props.steps);
   return (
-    <div className='flex flex-row'>
-      {props.steps.map((step) => (
-        <div key={step.name}>
-          <h2>{step.name}</h2>
-          <step.Component />
-        </div>
-      ))}
+    <div>
+      <div className='flex flex-row gap-6 w-full justify-center h-full '>
+        {props.steps.map((step) => (
+          <div key={step.name}>
+            <div className='rounded-lg bg-green-300'>{step.name}</div>
+          </div>
+        ))}
+      </div>
+      <button>{}</button>
     </div>
   );
 };

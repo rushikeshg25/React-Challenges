ae1f5a8d683c9aff55b4c5da66487f04c861486d
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-04-21 20:41:03 +0530

    Added TicTacToe's some Logic

--- src/Challenges/tic-tac-toe/index.tsx
+++ src/Challenges/tic-tac-toe/index.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useState } from "react";
 
 const TicTacToe = () => {
-  return <div>TicTacToe</div>;
+  const [gameFinished, setGameFinished] = useState(false);
+  const [winner, setWinner] = useState<string | null>(null);
+  const [board, setBoard] = useState<string[]>([
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+  ]);
+  const [currentTurn, setCurrentTurn] = useState<string>("X");
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1>Tic Tac Toe</h1>
+
+      <div className='grid grid-cols-3'>
+        {board.map((i: string, idx) => (
+          <div className='size-32 border-2 border-black' key={idx}>
+            {i !== "." && i}
+          </div>
+        ))}
+      </div>
+      {gameFinished && <div>{`${winner} wins!`}</div>}
+    </div>
+  );
 };
 
 export default TicTacToe;

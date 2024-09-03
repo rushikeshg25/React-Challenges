f04fa0938e417c72ef31382e784cf0ee300bce95
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-09-03 12:38:56 +0530

    feat:find-same-colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Home";
-import MultiStepFormProgress from "./Challenges/multistep-form-progress/Index";
-import GridSelect from "./Challenges/grid-select";
-import TicTacToe from "./Challenges/tic-tac-toe";
-import Landing from "./Landing";
-import Search from "./Challenges/search";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home';
+import MultiStepFormProgress from './Challenges/multistep-form-progress/Index';
+import GridSelect from './Challenges/grid-select';
+import TicTacToe from './Challenges/tic-tac-toe';
+import Landing from './Landing';
+import Search from './Challenges/search';
 
 const App = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: (
         <div className='text-5xl '>
           <Landing />
@@ -17,23 +17,27 @@ const App = () => {
       ),
     },
     {
-      path: "/multistep-form-progess",
+      path: '/multistep-form-progess',
       element: <MultiStepFormProgress />,
     },
     {
-      path: "/home",
+      path: '/home',
       element: <Home />,
     },
     {
-      path: "/grid-select",
+      path: '/grid-select',
       element: <GridSelect />,
     },
     {
-      path: "/tic-tac-toe",
+      path: '/tic-tac-toe',
       element: <TicTacToe />,
     },
     {
-      path: "/search",
+      path: '/search',
+      element: <Search />,
+    },
+    {
+      path: '/find-same-colors',
       element: <Search />,
     },
   ]);
---
+++ src/Challenges/find-same-colors/index.tsx
@@ -0,0 +1,5 @@
+const FindSameColors = () => {
+  return <></>;
+};
+
+export default FindSameColors;
